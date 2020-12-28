df208504ca789e8f439418330237605df8a6492b
Maclyn Afonso <>
2020-12-28 16:35:03 +0000

    log in/out listener added

--- amazon-clone/src/Components/App.js
+++ amazon-clone/src/Components/App.js
@@ -9,8 +9,35 @@ import Home from './Home';
 import Footer from './Footer';
 import Checkout from './Checkout';
 import Login from './Login';
+import { useStateValue } from '../StateProvider';
+import { useEffect } from 'react';
+import { auth } from '../firebase';
 
 function App() {
+
+  const [, dispatch] = useStateValue()
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      }
+      else {
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+    return () => {
+      //CLEANUP
+      unsubscribe()
+    }
+  }, [])
+
   return (
     <Router>
       <div className="app">
